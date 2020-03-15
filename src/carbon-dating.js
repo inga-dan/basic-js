f9344e2736eaea78d47aeead1acc54b78adbe148
Inga Danilova <aminga@yandex.ru>
2020-03-15 13:09:43 +0300

    feat: add count-cats solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,3 +5,15 @@ module.exports = function dateSample(/* sampleActivity */) {
   throw 'Not implemented';
   // remove line with error and write your code here
 };
+
+
+/*if (!sampleActivity || typeof sampleActivity != 'string') return false
+
+  sampleActivity = parseFloat(sampleActivity)
+  if (sampleActivity > MODERN_ACTIVITY ||
+      sampleActivity < 0 ||
+     !sampleActivity) return false
+
+  return Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity) *
+    HALF_LIFE_PERIOD / 0.693);
+};*/
