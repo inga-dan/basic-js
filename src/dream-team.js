7276316887d0bb566a37aaaf83dc1bae8c20bbc2
Inga Danilova <aminga@yandex.ru>
2020-03-20 15:57:45 +0300

    feat: add dream-team and hanoi solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let team = [];
+  if (Array.isArray(members) != true) {
+    return false;
+  }
+  for (var i = 0; i < members.length; i++) {
+    if (typeof members[i] == 'string') {
+      team.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+  return team.sort().join('');
 };
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-/*require('./test/count-cats.test');*/
-require('./test/carbon-dating.test');/*
+require('./test/count-cats.test');
+require('./test/carbon-dating.test');
 require('./test/dream-team.test');
 require('./test/extended-repeater.test');
 require('./test/what-season.test');
@@ -7,4 +7,4 @@ require('./test/vigenere-cipher.test');
 require('./test/hanoi-tower.test');
 require('./test/transform-array.test');
 require('./test/simple-chain.test');
-require('./test/recursive-depth.test');*/
+require('./test/recursive-depth.test');
